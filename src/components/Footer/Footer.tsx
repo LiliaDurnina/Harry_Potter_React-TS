80abf06760f1c5d5a2d5cc7fda2e98ba91be7eb8
gromova-daria <146058791+gromova-daria@users.noreply.github.com>
2025-12-05 20:10:10 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,59 +1,53 @@
-// src/components/Footer/Footer.tsx
-import React from 'react';
-import logo from '../../assets/images/logo.svg'; // Импортируем логотип
+import logo from '../../assets/images/logo.svg';
 
-const Footer: React.FC = () => {
-  // Функция для прокрутки вверх
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  };
+const developers = [
+  "Durnina Lilia",
+  "Gromova Daria",
+  "Chuprova Natalia",
+  "Mayorova Maria"
+];
+
+const contacts = [
+  "lsdurnina@edu.hse.ru",
+  "dsgromova@edu.hse.ru",
+  "npchuprova@edu.hse.ru",
+  "mvmaiorova@edu.hse.ru"
+];
+
+const Footer = () => {
+  const scrollToTop = () =>
+    window.scrollTo({ top: 0, behavior: "smooth" });
 
   return (
     <footer className="footer" id="contacts">
       <div className="footer__main">
         <div className="container">
           <div className="footer__columns">
+
+            {/* Логотип + описание */}
             <div className="footer__column">
-              <img 
-                src={logo} 
-                alt="Harry Potter logo" 
-                className="footer__logo" 
-              />
+              <img src={logo} alt="Harry Potter logo" className="footer__logo" />
               <p className="footer__text">
-                The website was created by students of educational groups 23CST-4 and 23CST-5. 
+                The website was created by students of educational groups 23CST-4 and 23CST-5.
                 We put our whole soul and even a few tears into it, working with our favorite childhood characters.
               </p>
             </div>
-            <div className="footer__column">
-              {/* Пустая колонка для сетки */}
-            </div>
-            <div className="footer__column">
-              <h3 className="footer__title">DEVELOPERS</h3>
-              <ul className="footer__list">
-                <li className="footer__list-item">Durnina Lilia</li>
-                <li className="footer__list-item">Gromova Daria</li>
-                <li className="footer__list-item">Chuprova Natalia</li>
-                <li className="footer__list-item">Mayorova Maria</li>
-              </ul>
-            </div>
-            <div className="footer__column">
-              <h3 className="footer__title">CONTACT US</h3>
-              <ul className="footer__list">
-                <li className="footer__list-item">lsdurnina@edu.hse.ru</li>
-                <li className="footer__list-item">dsgromova@edu.hse.ru</li>
-                <li className="footer__list-item">npchuprova@edu.hse.ru</li>
-                <li className="footer__list-item">mvmaiorova@edu.hse.ru</li>
-              </ul>
-            </div>
+
+            <div className="footer__column" />
+
+            {/* Developers */}
+            <FooterColumn title="DEVELOPERS" items={developers} />
+
+            {/* Contacts */}
+            <FooterColumn title="CONTACT US" items={contacts} />
+
           </div>
         </div>
       </div>
+
       <div className="footer__bottom">
         <div className="container">
-          <button 
+          <button
             className="footer__top-button"
             onClick={scrollToTop}
             aria-label="Scroll to top"
@@ -65,5 +59,24 @@ const Footer: React.FC = () => {
     </footer>
   );
 };
+
+const FooterColumn = ({
+  title,
+  items
+}: {
+  title: string;
+  items: string[];
+}) => (
+  <div className="footer__column">
+    <h3 className="footer__title">{title}</h3>
+    <ul className="footer__list">
+      {items.map((item) => (
+        <li key={item} className="footer__list-item">
+          {item}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Footer;
